2cbc3a9de89c488ed66c7f9217abafd193f02897
hossenmaruf <severusmaruf740@gmail.com>
2022-10-10 02:13:36 +0600

    ajax

---
+++ includes/Frontend/Enquiry.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+namespace test\plugin\includes\Frontend;
+
+
+/**
+ * Undocumented class
+ */
+class Enquiry
+{
+    //hello 
+
+
+function __construct()
+{
+add_shortcode('academy_enquiry', [$this, 'render_shortcode']);
+}
+
+/**
+* Undocumented function
+*
+* @param [type] $atts
+* @param string $content
+* @return string
+*/
+public function render_shortcode($atts, $content = '')
+{
+
+wp_enqueue_script('academy-enquiry-script');
+wp_enqueue_style('academy-enquiry-style');
+}
+}
+
+
+?>
