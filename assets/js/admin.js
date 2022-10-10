70f3707f4e0d92d51b5d783f96272174283b3534
hossenmaruf <severusmaruf740@gmail.com>
2022-10-10 06:45:05 +0600

    ajax

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,45 +1,37 @@
-;(function($){
+;(function($) {
 
+    $('table.wp-list-table.contacts').on('click', 'a.submitdelete', function(e) {
+        e.preventDefault();
 
-     $(table.wp-list-table.contacts).on('click' , 'a.submitdelete', function(e) {
-
-    e.preventDefault();
-
-        if(!confirm(test.confirm)) {
-            return ;
+        if (!confirm(test.confirm)) {
+            return;
         }
 
-        var self = $(this) ,
-        id = self.data('id') ;
+        var self = $(this),
+            id = self.data('id');
 
-        //   wp.ajax.send("wd-acdemy-deleted-contact" ,{
-
-        // data : {
-        //     id : id ,
-        //     _wpnonce = test.nonce 
-        // }
-
-        wp.ajax.post('m-academy-delete-contact' , {
-            id : id ,
-            _wpnonce : test.nonce  
+        // wp.ajax.send('wd-academy-delete-contact', {
+        //     data: {
+        //         id: id,
+        //         _wpnonce: weDevsAcademy.nonce
+        //     }
+        // })
+        wp.ajax.post('m-academy-delete-contact', {
+            id: id,
+            _wpnonce: test.nonce
         })
-        .done(function(response){
+        .done(function(response) {
 
             self.closest('tr')
-            .css('background-color' , 'red') 
-            .hide(600, function(){
-                $(this).remove() ;
-            }) ;
+                .css('background-color', 'red')
+                .hide(600, function() {
+                    $(this).remove();
+                });
 
-        }) 
+        })
         .fail(function() {
-            alert(test.error) ;
-        }) ;
-        
+            alert(test.error);
+        });
+    });
 
-    }) ;
-
-})
-
-
-
+})(jQuery);
