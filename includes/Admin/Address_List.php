10013e868f17bf1ba3336b0631e7718c0bd869f9
hossenmaruf <severusmaruf740@gmail.com>
2022-10-12 05:06:54 +0600

    cache &Rredis

--- includes/Admin/Address_List.php
+++ includes/Admin/Address_List.php
@@ -153,7 +153,7 @@ class Address_List extends \WP_List_Table
 
         $this->_column_headers = [$column, $hidden, $sortable];
 
-        $per_page     = 10;
+        $per_page     = 2;
         $current_page = $this->get_pagenum();
         $offset       = ($current_page - 1) * $per_page;
 
